d8562e67dee0bb4ee0abfab9927c1e5f6f3c9a8d
Diego Villamil <diego_villamil@outlook.com>
2018-03-21 18:14:35 -0600

    Más cambios a los tests funcionales.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
 
+import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase): 
 
@@ -15,16 +17,32 @@ class NewVisitorTest(unittest.TestCase):
     # Ana quiere entrar a la página del squash. 
     self.browser.get("http://localhost:8000")
 
-    # Ana verifica que el título diga juegos. 
+    # Ana verifica que el título y el header digan Juegos. 
     self.assertIn("Games played", self.browser.title)
+    header_text = self.browser.find_element_by_tag_name("h1").text
+    self.assertIn("Games played", header_text)    
+
+    # Hay un recuadro para ingresar juegos. 
+    inputbox = self.browser.find_element_by_id("id_new_item") 
+    self.assertEqual(inputbox.get_attribute("placeholder"), 
+        "Enter a game as:  'Score; AgainstPlayer'")
+
+    # Ana ingresa su primer juego contra Pablo. Pierde 6-11  
+    inputbox.send_keys("6-11; Pablo")
+
+    # Al darle enter se queda guardado el juego.
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = self.browser.find_element_by_id("id_list_table")
+    rows = table.find_elements_by_tag_name("tr")
+    self.assertTrue(
+      any(row.text == "1: 6-11; Pablo" for row in rows) )
+
+    # Todavía hay un cuadro para ingresar más juegos. 
+
     self.fail("Finish the test!")
-
-  # Ana ingresa su primer juego contra Pablo. Pierde 6-11
-
-  # Al darle enter se queda guardado el juego. 
-
-  # Todavía hay un cuadro para ingresar más juegos. 
-
+  
   # Ingresa un juego contra Paulina. Gana 11-8
 
   # La página se actualiza. Se ven los dos juegos. 
--- games/views.py
+++ games/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-  return HttpResponse("<html>  <title>Games played</title>  </html>")
+  response = HttpResponse("<html>  <title>Games played</title>  </html>")
+  return response
