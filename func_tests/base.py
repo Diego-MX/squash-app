d80e22ad1158cfcc1200c862eaef3fbee8098c43
Diego <dvillamil+gitkraken@gmail.com>
2019-03-04 22:35:51 -0600

    precreate session.  move login checks to base.

--- func_tests/base.py
+++ func_tests/base.py
@@ -18,13 +18,9 @@ class FunctionalTest(StaticLiveServerTestCase):
 
   def setUp(self): 
     self.browser   = webdriver.Firefox()
-    staging_server = os.environ.get("STAGING_SERVER")
-  
-    # LIVE_SERVER_URL ya existe en el SERVERTESTCASE, pero se modifica
-    # de acuerdo al STAGINGSERVER.   
+    staging_server = os.environ.get("STAGING_SERVER")  
     if staging_server: 
       self.live_server_url = "http://" + staging_server
-
 
   def tearDown(self):
     self.browser.quit()
@@ -40,27 +36,22 @@ class FunctionalTest(StaticLiveServerTestCase):
           raise an_error
         time.sleep(0.5)
 
-
   def check_for_row_in_game_table(self, row_text):
     table = self.browser.find_element_by_id("id_game_table")
     rows = table.find_elements_by_tag_name("tr")
     self.assertIn(row_text, [a_row.text for a_row in rows])
 
-  # def wait_for_row_in_game_table(self, row_text):
-  #   start_time = time.time()
-  #   while True:
-  #     try: 
-  #       table = self.browser.find_element_by_id("id_game_table")
-  #       rows = table.find_elements_by_tag_name("tr")
-  #       self.assertIn(row_text, [a_row.text for a_row in rows])
-  #       return
-  #     except (AssertionError, WebDriverException) as err:
-  #       if time.time() - start_time > MAX_WAIT:
-  #         raise err
-  #       time.sleep(0.5)
-
-
   def get_game_input_box(self):
     return self.browser.find_element_by_id('id_text')
+
+  def wait_for_being_logged_in(self, email): 
+    self.wait_for(lambda: self.browser.find_element_by_link_text("Log out"))
+    navbar = self.browser.find_element_by_css_selector(".navbar")
+    self.assertIn(email, navbar.text)
+
+  def wait_for_being_logged_out(self, email): 
+    self.wait_for(lambda: self.browser.find_element_by_name("email"))
+    navbar = self.browser.find_element_by_css_selector(".navbar")
+    self.assertNotIn(email, navbar.text)
 
 
